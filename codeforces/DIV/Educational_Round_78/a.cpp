0cb6da395ca2a6925f6c79dbc9c359a95e13242b
ashishihota <ashishhota79@gmail.com>
2020-01-10 19:11:27 +0530

    updated as for 10/01/2020

--- codeforces/DIV/Educational_Round_78/a.cpp
+++ codeforces/DIV/Educational_Round_78/a.cpp
@@ -10,6 +10,34 @@ const int MOD = 1e9 + 7;
 #define repp(i,a,b) for(i=a;i<b;++i)
 
 int main() {
+    int n;
+    cin >> n;
+    while(n--){
+        string p,h,ss;
+        int len ,lenn, countt = 0;
+        cin >> p >> h;
+        len = p.length();
+        lenn = h.length();
+        sort(p.begin(),p.end());
+        ss = h;
+        sort(ss.begin(),ss.end());
 
+        //cout << "---->sorted p = " << p<< endl;  //it is working
+        for(int i = 0;i < lenn; i++){
+            int num = 0;
+            if(h[i] == p[0] || ss[i == p[0]]){
+                string s = h.substr(i,len);
+                sort(s.begin(), s.end());
+                if(s.length() < len) continue;
+                //cout << " -->> the substr = "<< s << endl;
+                if(s == p){
+                    countt = 1;
+                    break;
+                }
+            }
+        }
+        if(countt == 1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
   return 0;
 }
--- codeforces/DIV/Educational_Round_78/b.cpp
+++ codeforces/DIV/Educational_Round_78/b.cpp
@@ -10,6 +10,29 @@ const int MOD = 1e9 + 7;
 #define repp(i,a,b) for(i=a;i<b;++i)
 
 int main() {
+    int n, sum =  0;
+    cin >> n;
+    std::vector<ll> v;
+    for(int i = 1; i < 100000; i++){
+        sum += i;
+        v.push_back(sum);
+    }
+    while (n--) {
+        int a, b, diff;
+        cin >> a >> b;
+        diff = abs(b - a);
+        cout << "diff = " << diff << endl;
+        for(int i = 0;i < v.size(); i++){
+            if(a == b){
+                cout <<"0"<< endl;
+                break;
+            }
+            if(v[i] == diff){
+                cout << i+ 1<<endl;
+                break;
+            }
+        }
 
+    }
   return 0;
 }
